24af9d72a43bf784caabe002107d8d494025d130
K <kk0412368@gmail.com>
2023-01-20 16:51:41 +0900

    1

---
+++ 0120.py
@@ -0,0 +1,21 @@
+# 10.1
+# class Thing():
+#     pass
+# print(Thing)
+#
+# class Thing():
+#     pass
+# example = Thing()
+# print(Thing)
+
+
+
+#10.2
+# class Thing2():
+#      pass
+# example=Thing2()
+# example.letters = "abc"
+# print(Thing2)
+
+
+
